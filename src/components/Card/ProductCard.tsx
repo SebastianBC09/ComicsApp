1f58ada6a62d298a5639d3fa332a4b46346daa18
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-09-15 00:56:39 -0500

    Feat: new component added, like icon for wishlist feature

--- src/components/Card/ProductCard.tsx
+++ src/components/Card/ProductCard.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonImg, IonCardTitle } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonImg, IonCardTitle, IonIcon } from '@ionic/react';
 import { Card } from '../../types/card';
+import { heart, heartOutline } from 'ionicons/icons';
 import './ProductCard.css';
 
-const ProductCard:React.FC<Card> = ({producto, seeDetails}) => {
+const ProductCard:React.FC<Card> = ({producto, seeDetails, isInWishlist, toggleWishlist}) => {
   return (
     <IonCard className='product-card'>
       <IonImg className="product-card--image" src={producto.imageURL} alt={producto.titulo}  />
+
+      <IonIcon
+        icon={isInWishlist ? heart : heartOutline}
+        className='wishlist-icon'
+        onClick={() => toggleWishlist(producto)}
+        color={isInWishlist ? 'danger' : 'medium'}
+      />
 
       <IonCardHeader>
         <IonCardTitle>{producto.titulo}</IonCardTitle>
