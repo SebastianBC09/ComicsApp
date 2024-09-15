ef73a76d8db7ee2b088fe0b76016c13a09d7ae19
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-09-15 00:56:22 -0500

    Feat: card type updated

--- src/types/card.ts
+++ src/types/card.ts
@@ -3,4 +3,6 @@ import { Producto } from "./producto";
 export interface Card {
   producto: Producto
   seeDetails: (id: number) => void
+  isInWishlist: boolean
+  toggleWishlist: (producto: Producto) => void
 }
