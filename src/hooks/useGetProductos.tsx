68eb9bbf1e216dc8435b04a02ae6933bbcff7c84
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-09-15 00:30:59 -0500

    Feat: new constant added to custom hook

--- src/hooks/useGetProductos.tsx
+++ src/hooks/useGetProductos.tsx
@@ -2,9 +2,11 @@ import React, {useEffect,useState} from 'react';
 import api from '../api/axiosConfig';
 import { AxiosResponse } from 'axios';
 import { Producto } from '../types/producto';
+import { Categoria } from '../types/categoria';
 
 export const useGetProductos = () => {
   const [productos, setProductos] = useState<Producto[]>([]);
+  const [categorias, setCategorias] = useState<Categoria[]>([]);
 
   const Productos = useEffect(() => {
     const FetchProductos = async () => {
@@ -18,8 +20,20 @@ export const useGetProductos = () => {
     FetchProductos();
   }, [])
 
+  const Categorias = useEffect(() => {
+    const FetchCategorias = async () => {
+      try {
+        const response: AxiosResponse<Categoria[]>  = await api.get('productos/categorias');
+        setCategorias(response.data);
+      } catch (error) {
+        console.error("Error al obtener las categorias", error);
+      }
+    };
+    FetchCategorias();
+  }, []);
 
   return {
-    productos
+    productos,
+    categorias
   }
 }
