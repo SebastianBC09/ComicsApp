9a9fab828d1373f6385c19d7a3f3300a41f29247
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-09-15 21:38:44 -0500

    Feat: minor change made to hook

--- src/hooks/useRegister.tsx
+++ src/hooks/useRegister.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
-import api from 'api/axiosConfig';
-import { AxiosResponse } from 'axios';
-import { RegisterData, RegisterResponse } from 'types/auth';
+import { RegisterData, RegisterResponse } from '../types/auth';
+import { register } from '../services/AuthService';
 
-export const useRegisterUser = () => {
+export const useRegister = () => {
   const [formData, setFormData] = useState<RegisterData>({
     nombreUsuario: '',
     email: '',
     passwordHash: ''
   });
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [registerResponse, setRegisterResponse] = useState<RegisterResponse | null>(null);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
@@ -19,16 +21,22 @@ export const useRegisterUser = () => {
 
   const handleSubmit = async (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
+    setLoading(true);
     try {
-      const response: AxiosResponse<RegisterResponse> = await api.post('/auth/registro', formData);
+      const response = await register(formData);
+      setRegisterResponse(response);
     } catch (error) {
       console.error("Error registrando al usuario", error);
+    } finally {
+      setLoading(false);
     }
   }
 
   return {
     formData,
     handleChange,
-    handleSubmit
+    handleSubmit,
+    loading,
+    registerResponse
   }
 }
