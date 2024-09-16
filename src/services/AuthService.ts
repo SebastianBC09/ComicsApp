06381a8da13cc159c5ba17d391ed5e1f876591da
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-09-15 21:33:25 -0500

    Fix: auth service bugs fixed

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,17 +1,18 @@
 import api from '../api/axiosConfig';
+import { LoginData, RegisterData } from '../types/auth';
 
-export const login = async (email: string, password: string) => {
+export const login = async (formData: LoginData) => {
   try {
-    const response = await api.post('/auth/login', { email, password });
+    const response = await api.post('/auth/login', formData);
     return response.data;
   } catch (error) {
     throw error;
   }
 };
 
-export const register = async (nombreUsuario: string, email: string, password: string) => {
+export const register = async (formData: RegisterData) => {
   try {
-    const response = await api.post('/auth/registro', { nombreUsuario, email, password });
+    const response = await api.post('/auth/registro', formData);
     return response.data;
   } catch (error) {
     throw error;
